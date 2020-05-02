69560bd9e1fdba0f96f8d6c21c464fc1436070ef
LordRonz <aarontanhar2000@gmail.com>
2020-05-02 18:55:41 +0700

    damn

--- portfolio.js
+++ portfolio.js
@@ -21,6 +21,34 @@ sr.reveal('#hobbies', {
     origin: 'right',
     viewFactor: 0.2
 });
+sr.reveal('#card1', {
+    duration: 2000,
+    distance: '500px',
+    origin: 'left',
+    viewFactor: 0.2
+});
+sr.reveal('#card2', {
+    duration: 2000,
+    distance: '500px',
+    origin: 'bottom',
+    viewFactor: 0.2
+});
+sr.reveal('#card3', {
+    duration: 2000,
+    distance: '500px',
+    origin: 'top',
+    viewFactor: 0.2
+});
+sr.reveal('#card4', {
+    duration: 2000,
+    distance: '500px',
+    origin: 'right',
+    viewFactor: 0.2
+});
+sr.reveal('#education', {
+    duration: 2000,
+    viewFactor: 0.2
+});
 
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
